283815d0d0393192e9afc282ede319360599da76
komal devnani <komaldevnani00@gmail.com>
2020-05-19 22:35:13 +0530

    intro and contact update

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,8 @@ import Gallery from '../components/Gallery'
 import Layout from '../components/layout'
 
 const HomeIndex = () => {
-  const siteTitle = 'Gatsby Starter - Strata'
-  const siteDescription = 'Site description'
+  const siteTitle = 'Komal Devnani | Personal blog'
+  const siteDescription = 'Komal Devnani is a web developer with excellent design sense.'
 
   return (
     <Layout>
@@ -19,18 +19,13 @@ const HomeIndex = () => {
         <section id="one">
           <header className="major">
             <h2>
-              Ipsum lorem dolor aliquam ante commodo
-              <br />
-              magna sed accumsan arcu neque.
+              Get to know me before dive into my content
             </h2>
           </header>
           <p>
-            Accumsan orci faucibus id eu lorem semper. Eu ac iaculis ac nunc
-            nisi lorem vulputate lorem neque cubilia ac in adipiscing in curae
-            lobortis tortor primis integer massa adipiscing id nisi accumsan
-            pellentesque commodo blandit enim arcu non at amet id arcu magna.
-            Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate
-            lorem neque cubilia.
+            I am a computer science undergraduate student and passionate about web development. 
+            I like to dig deep into the concepts and foundations. The languages i focus most on are Python and Ruby with having some knowledge of java and c++.
+            I'am happy to welcome you on my blog site. 
           </p>
           <ul className="actions">
             <li>
@@ -62,7 +57,8 @@ const HomeIndex = () => {
           </p>
           <div className="row">
             <div className="8u 12u$(small)">
-              <form method="post" action="#">
+              <form method="post" netlify-honeypot="bot-field" data-netlify="true">
++               <input type="hidden" name="bot-field" />
                 <div className="row uniform 50%">
                   <div className="6u 12u$(xsmall)">
                     <input
